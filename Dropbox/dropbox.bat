d419f4a42adfb2ed195e4e9b6e325eac168250ef
tafewollongbarsys <39718635+tafewollongbarsys@users.noreply.github.com>
2019-07-19 08:24:04 +1000

    Update dropbox.bat
    Updated binary version.

--- Dropbox/dropbox.bat
+++ Dropbox/dropbox.bat
@@ -70,7 +70,7 @@ set LOGFILE=
 
 REM | Package to install.
 REM | - Do not use trailing slashes (\).
-set BINARY=Dropbox 73.4.118 Offline Installer.exe
+set BINARY=Dropbox 77.4.131 Offline Installer.exe
 set FLAGS=/S
 
 REM | ========
